616e83bcfec4507e0414d110962a5d4d0863adb6
Mind-thatsall <mathieurossi77@gmail.com>
2023-03-02 22:44:28 +0100

    added: composable useFetch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { useFetch } from "../utils/fetch";
+
+const { data, error } = useFetch("http://localhost:9090/api/manhwas/1");
+</script>
+
 <template>
   <main>Hello World!</main>
 </template>
